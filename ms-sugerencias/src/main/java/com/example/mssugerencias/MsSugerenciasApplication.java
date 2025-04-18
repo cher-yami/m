6795b485089a14dca3acff370e3c99eb09d0a976
cher-yami <chermanfreefire@gmail.com>
2025-04-18 16:21:39 -0500

    Add files via upload

---
+++ ms-sugerencias/src/main/java/com/example/mssugerencias/MsSugerenciasApplication.java
@@ -0,0 +1,13 @@
+package com.example.mssugerencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSugerenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSugerenciasApplication.class, args);
+	}
+
+}
---
+++ ms-sugerencias/src/test/java/com/example/mssugerencias/MsSugerenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mssugerencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSugerenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
